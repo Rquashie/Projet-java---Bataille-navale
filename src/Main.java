5f858ca43cba25ec2852078070d695af338cdf25
rQuashie <kangoojuniors93@gmail.com>
2024-12-24 18:33:09 +0100

    feat : Préparation des grilles de jeu pour les joueurs

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scannerJun = new Scanner(System.in);
+        Scanner scannerJdeux = new Scanner(System.in);
+        String pseudoJun, pseudoJdeux = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Pseudo joueur 1 : ");
+        pseudoJun = scannerJun.nextLine();
+
+
+        //Les grilles
+        String[][] grilleJun = new String[10][10];
+        String[][] grilleJdeux = new String[10][10];
+
+        for (int i = 0; i < grilleJun.length; i++) {
+            for (int j = 0; j < grilleJun.length; j++) {
+                grilleJun[i][j] = "~";
+                grilleJdeux[i][j] = "~";
+            }
+        }
+        for (int i = 0; i < grilleJun.length; i++) {
+            System.out.print((Arrays.deepToString(grilleJun[i]))+' '+Arrays.deepToString(grilleJdeux[i])+'\n');
+
         }
     }
 }
+
+
